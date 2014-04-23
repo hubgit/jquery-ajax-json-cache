4964b40009dd9e876df4233a91a7097494c85f8d
Alf Eaton <alf@hubmed.org>
2014-04-23 09:40:31 +0100

    Update demo.js

--- demo/demo.js
+++ demo/demo.js
@@ -1,6 +1,5 @@
 $(function() {
   $.getJSON('http://www.bbc.co.uk/bbcone/programmes/schedules/london/today.json', function(data) {
-    console.log(data);
     $('textarea').html(JSON.stringify(data, null, '  '));
   });
 });
