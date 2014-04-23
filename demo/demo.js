42c8c21cb11b189214d7a1b1672df30b92ccd219
Alf Eaton <hubgit@@users.noreply.github.com>
2014-04-23 09:48:26 +0100

    simplify demo

--- demo/demo.js
+++
@@ -1,5 +0,0 @@
-$(function() {
-  $.getJSON('http://www.bbc.co.uk/bbcone/programmes/schedules/london/today.json', function(data) {
-    $('textarea').html(JSON.stringify(data, null, '  '));
-  });
-});
